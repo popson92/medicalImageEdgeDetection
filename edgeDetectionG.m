291654d36e5d170936de6f4ccb6ad228b85c4a40
Popoola Oluwasola <popoolaolusola92@gmail.com>
2021-08-28 09:07:06 +0100

    final work

--- edgeDetectionG.m
+++ edgeDetectionG.m
@@ -22,7 +22,7 @@ function varargout = edgeDetectionG(varargin)
 
 % Edit the above text to modify the response to help edgeDetectionG
 
-% Last Modified by GUIDE v2.5 16-Mar-2021 19:37:25
+% Last Modified by GUIDE v2.5 27-May-2021 00:19:45
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -85,15 +85,22 @@ function loadImage_Callback(hObject, eventdata, handles)
 
 [filename, pathname] = uigetfile('*.*', 'Pick a MATLAB code file');
 if isequal(filename,0) || isequal(pathname,0)
-   disp('You pressed cancel')
+   errordlg('You pressed cancel')
 else
    filename = strcat(pathname, filename);
-   img_R = imread(filename);
+   [img_R, map ]= imread(filename);
+    if(isempty(map)) % image is RGB or grayscale
+        if(size(img_R, 3) == 1) % image is grayscale
+            img_R = cat(3, img_R, img_R, img_R);
+        end
+    else % image is indexed
+        img_R = ind2rgb(img_R, map);
+    end
    axes(handles.axes1);
-   imshow(img_R);
+   imshow(img_R, map);
+   title('MRI RGB IMAGE');
 end
 handles.img_R = img_R;
-
 %Updating handles Structure
 guidata(hObject, handles);
 
@@ -106,22 +113,29 @@ function listbox1_Callback(hObject, eventdata, handles)
 % Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from listbox1
 img_R = handles.img_R;
+%map = handles.map;
 value = get(hObject,'Value');
 switch value
     case 1
         img = rgb2gray(img_R);
-        p_x=[-1 0 1;-1 0 1;-1 0 1];
+        p_x=[-1 0 1;-2 0 2;-1 0 1];
         imgx=filter2(p_x,img);
-        p_y=p_x';
+        p_y=p_x'; 
         imgy=filter2(p_y,img);
         edge = sqrt(imgx.^2+imgy.^2);
         edge_thresholding = imbinarize(edge/255,0.8);
+        img = double(img);
+        img_2 = double(edge_thresholding);
+        [peaksnr, snr] = psnr(img_2, img);
+        peaksnr = num2str(abs(peaksnr));
+        set(handles.edit2, 'String', peaksnr);
         axes(handles.axes2);
         imshow(edge_thresholding);
+        title('EDGE DETECTED');
     case 2
-        Nx1=10;Sigmax1=2;Nx2=10;Sigmax2=4;Theta1=pi/2;
-        Ny1=10;Sigmay1=2;Ny2=10;Sigmay2=4;Theta2=pi/2;
-        alfa=0.9;
+        Nx1=08;Sigmax1=2;Nx2=08;Sigmax2=4;Theta1=pi/2;
+        Ny1=08;Sigmay1=2;Ny2=08;Sigmay2=4;Theta2=pi/2;
+        alfa=0.7;
         img = rgb2gray(img_R);
         filterx=d2dgauss(Nx1,Sigmax1,Nx2,Sigmax2,Theta1);
         Ix= conv2(img,filterx,'same');
@@ -133,11 +147,32 @@ switch value
         level=alfa*(I_max-I_min)+I_min;
         
         Ibw=max(NVI,level.*ones(size(NVI)));
-        axes(handles.axes2);
-        %Ibw = imadd(Ibw, 1000);
+        Ibw = imadd(Ibw, 100000);
+        img = double(img);
+        img_2 = double(Ibw);
+        [peaksnr, snr] = psnr(img_2, img);
         %Ibw = mat2gray(Ibw);
+        set(handles.edit2, 'String', abs(peaksnr));
+        axes(handles.axes2);
         imshow(Ibw, []);
+        title('EDGE DETECTED');
         %colormap('gray');
+    case 3
+        img = rgb2gray(img_R);
+        hprewittx = [1 0 1;-1 0 1;1 0 1];
+        hprewitty = hprewittx';
+        prewitt_x = filter2(hprewittx, img);
+        prewitt_y = filter2(hprewitty, img);
+        prewittx_y = sqrt(prewitt_x.^2 + prewitt_y.^2);
+        edge_pre = imbinarize(prewittx_y/255);
+        img = double(img);
+        img_2 = double(edge_pre);
+        [peaksnr, snr] = psnr(img_2, img);
+        peaksnr = num2str(abs(peaksnr));
+        set(handles.edit2, 'String', peaksnr);
+        axes(handles.axes2);
+        imshow(edge_pre);
+        title('EDGE DETECTED');
 end
 function h = d2dgauss(n1,sigma1,n2,sigma2,theta)
 r=[cos(theta) -sin(theta);
@@ -167,6 +202,52 @@ function listbox1_CreateFcn(hObject, eventdata, handles)
 % handles    empty - handles not created until after all CreateFcns called
 
 % Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
